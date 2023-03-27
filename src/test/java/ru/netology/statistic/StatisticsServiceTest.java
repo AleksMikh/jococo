e00463ebbce9e8717258f2d9967c3800107e066f
Mikhail Alekseev <rai1gun@mail.ru>
2023-03-27 22:47:44 +0300

    ReformatCode

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,6 +17,7 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     void findLargestAmongNegative() {
         StatisticsService service = new StatisticsService();
@@ -28,6 +29,7 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     void findMax2() {
         StatisticsService service = new StatisticsService();
